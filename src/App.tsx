3e2bacb52b748b26354cbdf0d16da1ed89c706b1
Alex Gilbert <alex.js.ltd@gmail.com>
2022-11-16 10:49:19 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { useAuth } from './context/auth-context'
 import { FullPageSpinner } from 'comps/library'
 
+// authenticated app doesn't load until the user has logged in
 const AuthenticatedApp = React.lazy(() => import('./authenticated-app'))
 const UnauthenticatedApp = React.lazy(() => import('./unauthenticated-app'))
 
